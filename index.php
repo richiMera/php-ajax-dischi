90ae173c6f3492ed1548218f801611bd76af510e
Riccardo Ferrari <richi.f93@gmail.com>
2021-01-22 15:17:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  include __DIR__ . '/db.php'
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <link rel="stylesheet" href="css/app.css">
+     <title>PHP Dischi</title>
+   </head>
+   <body>
+    <?php include __DIR__ . '/partials/header.php' ?>
+    <?php include __DIR__ . '/partials/main.php' ?>
+   </body>
+ </html>
---
+++ partials/header.php
@@ -0,0 +1,4 @@
+
+<header>
+  <img src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-Logo.png" alt="">
+</header>
